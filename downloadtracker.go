ba4668427cc1590e80d37e0bb2d9d74d30f0bd71
William <williamqmckenzie@gmail.com>
2025-12-11 20:02:23 -0500

    corsy

--- downloadtracker.go
+++ downloadtracker.go
@@ -12,6 +12,7 @@ func enableCors(w http.ResponseWriter) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	w.Header().Set("Content-Type", "application/json")
 }
 
 func download(w http.ResponseWriter, r *http.Request) {
