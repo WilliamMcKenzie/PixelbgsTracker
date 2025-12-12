db1a679118ec34597fd38d37cec3256cffde0aaa
William <williamqmckenzie@gmail.com>
2025-12-11 20:12:31 -0500

    ALL ALLOWED HEADERS

--- downloadtracker.go
+++ downloadtracker.go
@@ -10,8 +10,8 @@ import (
 
 func enableCors(w http.ResponseWriter) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 }
 
 func download(w http.ResponseWriter, r *http.Request) {
